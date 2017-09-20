da0ea48200953770c464dd312a4fec60aea962a1
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-09-20 18:31:25 +0200

    input event

--- src/app.js
+++ src/app.js
@@ -4,12 +4,29 @@ import Rx from 'rxjs/Rx';
 console.log('RxJS Boiler Running...');
 
 const btn = $('#btn');
+const input = $('#input');
+const output = $('#output');
 
 const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
 
 btnStream$.subscribe(
     function (e) {
         console.log(e.target.innerHTML);
+    },
+    function (err) {
+        console.log(err);
+    },
+    function () {
+        console.log('Completed');
+    }
+)
+
+const inputStream$ = Rx.Observable.fromEvent(input, 'keyup');
+
+inputStream$.subscribe(
+    function (e) {
+        console.log(e.target.value);
+        output.append(e.target.value);
     },
     function (err) {
         console.log(err);
